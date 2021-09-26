8262e5f6a56c5f154bceaa348a2212756efa8513
david <davidabu91@gmail.com>
2021-09-26 17:04:45 +0300

    working on homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,9 @@ export default function Home() {
     <main>
         <img src={image} alt="" className="absolute object-cover w-full h-full"/>
         <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-            <h1 className="text-4xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hello. I'm David.</h1>
+            <h1 className="text-4xl font-bold text-green-50 leading-none lg:leading-snug lg:home-name">Hello. I'm David.</h1>
         </section>
     </main>
   );
 }
+//  text-4xl text-green-50  lg:font-bold cursive leading-none lg:leading-snug home-name

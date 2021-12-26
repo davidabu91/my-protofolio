c8bee16192489eb2b879c9654f5623f702562ff6
david <davidabu91@gmail.com>
2021-12-26 17:25:35 +0200

    Change hompage content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import img from '../assets/pexels-jakub-novacek-924824.jpg';
-import reactIcon from '../assets/react-icon.png';
-import cssIcon from '../assets/css3.png';
-import githubIcon from '../assets/github.png';
-import hrokuIcon from '../assets/heroku.png';
-import htmlIcon from '../assets/html5.png';
-import jsIcon from '../assets/javascript.png';
-import netlifyIcon from '../assets/netlify.png';
-import nodeIcon from '../assets/nodejs.png';
-import reduxIcon from '../assets/redux.png';
-import tailwiindIcon from '../assets/tailwind.png';
-import tsIcon from '../assets/typescript.png';
-
-
+import img from "../assets/pexels-jakub-novacek-924824.jpg";
+import reactIcon from "../assets/react-icon.png";
+import cssIcon from "../assets/css3.png";
+import githubIcon from "../assets/github.png";
+import hrokuIcon from "../assets/heroku.png";
+import htmlIcon from "../assets/html5.png";
+import jsIcon from "../assets/javascript.png";
+import netlifyIcon from "../assets/netlify.png";
+import nodeIcon from "../assets/nodejs.png";
+import reduxIcon from "../assets/redux.png";
+import tailwiindIcon from "../assets/tailwind.png";
+import tsIcon from "../assets/typescript.png";
 
 export default function Home() {
   return (
@@ -24,33 +22,37 @@ export default function Home() {
         </h1>{" "}
         <br />
         <h6 className="lg:text-3xl   text-2xl text-green-50 font-bold  leading-none lg:leading-snug ">
-          Graduated student searching for my first opportunity as a Front-End developer.{" "}
+          {/* Graduated student searching for my first opportunity as a Front-End developer.{" "}
           <br />
           I have two years of code experience in website
           design and MERN design concept. <br /> Knowledge and skills in front
           and back-end web development. Experience in a variety of areas and the
-          ability to learn independently and quickly.
+          ability to learn independently and quickly. */}
+          Operations Manager with 6+ years of experience in project management
+          and process leadership. <br /> I am changing my career and looking for an
+          opportunity as a <br /> <strong className="text-green-200 lg:text-4xl">FRONT END DEVELOPER.</strong> <br /> My motto: "Automate frequent
+          actions and take time to create new opportunities." <br /> Team player, open
+          mind and excellent communication skills. <br /> Organized, eager to learn new
+          things, loves to explore and solve problems.
         </h6>
-        <div className="bg-white opacity-30 mt-4 p-4 rounded-xl">
-        <div className="flex flex-row  justify-center ">
-        <img src={reactIcon} alt="" className="w-1/12 ml-8"/>
-        <img src={cssIcon} alt="" className="w-1/12 ml-8"/>
-        <img src={htmlIcon} alt="" className="w-1/12 ml-8"/>
-        <img src={jsIcon} alt="" className="w-1/12 ml-8"/> 
-        <img src={tsIcon} alt="" className="w-1/12 ml-8"/>
-        <img src={nodeIcon} alt="" className="w-1/12 ml-8"/>
-        </div>
-        <div className="flex flex-row  justify-center mt-8">
-        <img src={githubIcon} alt="" className="w-1/12 ml-8"/>
-        <img src={reduxIcon} alt="" className="w-1/12 ml-8"/>
-        <img src={tailwiindIcon} alt="" className="w-1/12 ml-8"/> 
-        <img src={hrokuIcon} alt="" className="w-1/12 ml-8"/>
-        <img src={netlifyIcon} alt="" className="w-1/12 ml-8"/>
-        </div>
-
+        <div className="bg-white opacity-30 mt-4 mb-4 p-4 rounded-xl">
+          <div className="flex flex-row  justify-center ">
+            <img src={reactIcon} alt="" className="w-1/12 ml-8" />
+            <img src={cssIcon} alt="" className="w-1/12 ml-8" />
+            <img src={htmlIcon} alt="" className="w-1/12 ml-8" />
+            <img src={jsIcon} alt="" className="w-1/12 ml-8" />
+            <img src={tsIcon} alt="" className="w-1/12 ml-8" />
+            <img src={nodeIcon} alt="" className="w-1/12 ml-8" />
+          </div>
+          <div className="flex flex-row  justify-center mt-8">
+            <img src={githubIcon} alt="" className="w-1/12 ml-8" />
+            <img src={reduxIcon} alt="" className="w-1/12 ml-8" />
+            <img src={tailwiindIcon} alt="" className="w-1/12 ml-8" />
+            <img src={hrokuIcon} alt="" className="w-1/12 ml-8" />
+            <img src={netlifyIcon} alt="" className="w-1/12 ml-8" />
+          </div>
         </div>
       </section>
-
     </main>
   );
 }
